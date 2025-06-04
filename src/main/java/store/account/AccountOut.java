56a125e0b0fe601bb86426876d3f0117708eef21
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 21:39:33 -0300

    fix: serialização aos DTOs usados em cache Redis

--- src/main/java/store/account/AccountOut.java
+++ src/main/java/store/account/AccountOut.java
@@ -4,7 +4,8 @@ import lombok.Builder;
 import lombok.experimental.Accessors;
 
 @Builder @Accessors(fluent = true)
-public record AccountOut(
+public record AccountOut implements Serializable(
+    private static final long serialVersionUID = 1L;
     String id,
     String name,
     String email
