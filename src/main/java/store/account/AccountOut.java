95e7977a21d45b6436217fcf55e6ed7d0026e65a
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 21:46:08 -0300

    fix: definição de record arrumada

--- src/main/java/store/account/AccountOut.java
+++ src/main/java/store/account/AccountOut.java
@@ -1,13 +1,13 @@
 package store.account;
 
+import java.io.Serializable;
 import lombok.Builder;
-import lombok.experimental.Accessors;
 
-@Builder @Accessors(fluent = true)
-public record AccountOut implements Serializable(
-    private static final long serialVersionUID = 1L;
+@Builder
+public record AccountOut(
     String id,
     String name,
     String email
-) {
+) implements Serializable {
+    private static final long serialVersionUID = 1L;
 }
